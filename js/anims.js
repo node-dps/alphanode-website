d65e2b32c2732a1a786499fc2bc3435ca232be9d
Pratyush Kumar <pk1234pratyushk@gmail.com>
2018-07-01 23:36:19 +0530

    Updated the NODE site - Pratyush

--- js/anims.js
+++ js/anims.js
@@ -1,24 +1,40 @@
 function onLoadAnimations(){
-    var a = document.querySelector('#reveal');
-    var b = document.querySelector('#reveal img');
-    var c = document.querySelector('#reveal h1');
-    var arrow = document.querySelector('#down-arrow');
+    var a = document.querySelectorAll('#reveal');
+    var b = document.querySelectorAll('#reveal img');
+    var c = document.querySelectorAll('#reveal h1');
+    var arrow = document.querySelectorAll('#down-arrow');
 
-
-    arrow.style.opacity = "1";
-    if(window.innerWidth > 1024){
-        arrow.style.bottom = "40px";
-    }else{
-        arrow.style.bottom = "70px";
+    if(arrow.length > 0){
+        for(var i = 0; i < arrow.length; i++){
+            arrow[i].style.opacity = "1";
+            if(window.innerWidth > 1024){
+                arrow[i].style.bottom = "40px";
+            }else{
+                arrow[i].style.bottom = "10vh";
+            }
+        }
     }
 
-    a.style.width = "100vw";
-    a.style.height = "100vw";
-    a.style.backgroundColor = "#ecf0f100";
+    if(a.length > 0){
+        for(var i = 0; i < a.length; i++){
+            a.style.width = "100vw";
+            a.style.height = "100vw";
+            a.style.backgroundColor = "#ecf0f100";
+        }
+    }
 
-    b.style.opacity = "1";
-    b.style.bottom = "0";
+    if(b.length > 0){
+        for(var i = 0; i < b.length; i++){
+            b.style.opacity = "1";
+            b.style.bottom = "0";
+        }
+    }
     
-    c.style.opacity = "1";
-    c.style.bottom = "0";
+    if(c.length > 0){
+        for(var i = 0; i < c.length; i++){
+            c.style.opacity = "1";
+            c.style.bottom = "0";
+        }
+    }
+
 }
